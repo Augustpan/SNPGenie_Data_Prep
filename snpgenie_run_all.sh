c0457475f85a17980a72a43feccdc0cb9fa93f21
潘远飞 Yuanfei Pan <pancat43@126.com>
2021-08-25 10:09:11 +0800

    Update snpgenie_run_all.sh

--- snpgenie_run_all.sh
+++ snpgenie_run_all.sh
@@ -3,8 +3,11 @@
 mkdir results
 for line in $(cat manifest_snpgenie)
 do
-    /home/dell2/yfpan/SNPGenie/snpgenie.pl --minfreq=0.01 --snpreport=snpgenie_input_files/$line.vcf --vcfformat 3 --fastafile=snpgenie_input_files/$line.fasta --gtffile=snpgenie_input_files/$line.gtf
-
-    cp -r SNPGenie_Results results/$line
-    rm -rf SNPGenie_Results
+    /home/dell2/yfpan/SNPGenie/snpgenie.pl \
+        --minfreq=0.01 \
+        --snpreport=snpgenie_input_files/$line.vcf \
+        --vcfformat=4 \
+        --fastafile=snpgenie_input_files/$line.fasta \
+        --gtffile=snpgenie_input_files/$line.gtf \
+        --outdir results/$line
 done
